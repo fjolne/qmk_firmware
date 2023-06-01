3a9441e1e4430a646a4bd3831c5fa5d7d8679c3b
Oleg Martynov <fjolne.yngling@gmail.com>
2023-06-01 16:46:13 +0100

    added num toggle to game layer

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -22,7 +22,7 @@ U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_NAV,KC_SPC),  LT(U
 KC_LALT,   KC_Q,   KC_W,      KC_E,      KC_R,             KC_T,   KC_Y,    KC_U,      KC_I,      KC_O,     \
 KC_LSFT,   KC_A,   KC_S,      KC_D,      KC_F,             KC_G,   KC_H,    KC_J,      KC_K,      KC_L,     \
 KC_LCTL,   KC_Z,   KC_X,      KC_C,      KC_V,             KC_B,   KC_N,    KC_M,      KC_COMM,   KC_DOT,   \
-U_NP,      U_NP,   KC_ESC,    KC_SPC,    KC_TAB,           KC_ENT, KC_BSPC, KC_DEL,    U_NP,      U_NP
+U_NP,      U_NP,   KC_ESC,    KC_SPC,    LT(U_NUM,KC_TAB), KC_ENT, KC_BSPC, KC_DEL,    U_NP,      U_NP
 #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
 
 #define MIRYOKU_LAYER_TAP \
