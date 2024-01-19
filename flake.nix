4e3e8228cbc730f82a250d3cae4ed4982e9f9de3
Oleg Martynov <fjolne.yngling@gmail.com>
2024-01-19 20:27:47 +0000

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Minimal template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              qmk
+            ];
+            shellHook = ''
+              echo hello
+            '';
+          };
+        };
+      });
+}
